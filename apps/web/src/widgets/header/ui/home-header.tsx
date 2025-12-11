61ff346c6060baf3fbff69282f3d46f904558560
Jonggwang Park <jgpark7281@gmail.com>
2025-12-11 16:29:32 +0900

    feat: add header, footer components

---
+++ apps/web/src/widgets/header/ui/home-header.tsx
@@ -0,0 +1,30 @@
+import { ModeToggle } from "@/shared/ui"
+import { IconMenu2 } from "@tabler/icons-react"
+
+import { Button } from "@workspace/ui/components/button"
+
+export const HomeHeader = () => {
+  return (
+    <header className="fixed top-0 left-0 right-0 z-50 px-6 py-6 md:px-12 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-2">
+          <span className="text-primary">
+            —
+          </span>
+          <span className="uppercase text-lg font-light tracking-widest">
+            Calelix
+          </span>
+        </div>
+
+        <div className="flex items-center gap-2">
+          <Button variant="ghost" className="size-9">
+            <IconMenu2 className="size-5" />
+          </Button>
+          <ModeToggle />
+        </div>
+      </div>
+
+      <div className="absolute top-20 left-0 right-0 h-px bg-gradient-to-r from-transparent via-primary to-transparent opacity-30" />
+    </header>
+  )
+}

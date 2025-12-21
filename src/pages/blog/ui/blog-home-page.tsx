ef894b146174af5ff8b8312b74c598f7f0c32e2c
Jonggwang Park <jgpark7281@gmail.com>
2025-12-21 17:07:23 +0900

    feat: add header and intro to blog home page

--- src/pages/blog/ui/blog-home-page.tsx
+++ src/pages/blog/ui/blog-home-page.tsx
@@ -1,7 +1,20 @@
+import { Header } from "@/widgets/header"
+
 export const BlogHomePage = () => {
+
   return (
-    <div>
-      <h1>Blog Home Page</h1>
-    </div>
+    <>
+      <Header className="py-14" />
+      <main className="w-full max-w-4xl mx-auto h-svh">
+        <div className="flex flex-col">
+          <h1 className="text-base font-bold">
+            Powered by Caffeine & Confusion
+          </h1>
+          <p className="mt-4 text-xs/relaxed text-muted-foreground">
+            Can{"'"}t start my day without Americano. For real. One cup to wake up, two to function, three for... just habit. This is a space for caffeine lovers.
+          </p>
+        </div>
+      </main>
+    </>
   )
 }

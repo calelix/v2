d44dc63b663889f3b176f728ea9fc9efd5f8e703
Jonggwang Park <jgpark7281@gmail.com>
2025-12-27 06:31:41 +0900

    feat: update home page

--- src/pages/home-page/ui/home-page.tsx
+++ src/pages/home-page/ui/home-page.tsx
@@ -2,7 +2,8 @@
 
 import { useState } from "react"
 
-import { Button } from "@/shared/ui/shadcn/button"
+import { motion } from "framer-motion"
+
 import { HomePageIntro } from "./home-page-intro"
 
 export const HomePage = () => {
@@ -19,10 +20,38 @@ export const HomePage = () => {
   }
 
   return (
-    <div className="flex flex-col items-center justify-center h-svh">
-      <Button>
-        Hello World
-      </Button>
+    <div
+      className="px-4 lg:px-0"
+      style={
+        {
+          "--header-height": "calc(var(--spacing)*32)",
+          "--footer-height": "calc(var(--spacing)*32)",
+        } as React.CSSProperties
+      }
+    >
+      <div aria-hidden="true" className="h-32" />
+      <main className="w-full max-w-2xl mx-auto min-h-[calc(100svh-var(--header-height)-var(--footer-height))]">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1.25 }}
+        >
+          <motion.div
+            initial={{ opacity: 0, y: 25 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.75, delay: 0.25 }}
+            className="text-center"
+          >
+            <h1 className="text-lg font-bold">
+              Welcome !
+            </h1>
+            <p className="mt-4 text-xs/relaxed text-muted-foreground">
+              I take my time here. This place isn{"'"}t in a hurry.
+            </p>
+          </motion.div>
+        </motion.div>
+      </main>
+      <div aria-hidden="true" className="h-32" />
     </div>
   )
 }

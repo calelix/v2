640d89547cabc064446ef37dfe5120c128184f4a
Jonggwang Park <jgpark7281@gmail.com>
2026-01-26 21:45:31 +0900

    chore: install tanstack/react-query, devtools, vercel/blob

--- src/shared/ui/shadcn/spinner.tsx
+++ src/shared/ui/shadcn/spinner.tsx
@@ -1,9 +1,9 @@
+import { IconLoader2 } from "@tabler/icons-react"
 import { cn } from "@/shared/lib/utils/tailwindcss"
-import { IconLoader } from "@tabler/icons-react"
 
 function Spinner({ className, ...props }: React.ComponentProps<"svg">) {
   return (
-    <IconLoader role="status" aria-label="Loading" className={cn("size-4 animate-spin", className)} {...props} />
+    <IconLoader2 role="status" aria-label="Loading" className={cn("size-4 animate-spin", className)} {...props} />
   )
 }
 

b74cb0908fb18f4c7db9704f01f2022a5934a6a7
Jonggwang Park <jgpark7281@gmail.com>
2026-01-26 23:41:41 +0900

    chore: allow vercel blob storage domain in remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [{
+      protocol: "https",
+      hostname: "gz0bl3nb3hcuttnc.public.blob.vercel-storage.com",
+    }],
+  },
 }
 
 export default nextConfig

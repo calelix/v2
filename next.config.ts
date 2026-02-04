2ccefa50335a8c2275f0cdf700f4196f029d4d73
Jonggwang Park <jgpark7281@gmail.com>
2026-02-04 22:15:43 +0900

    build: react compiler 설정 추가

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next"
 
+const isDevelopment = process.env.NODE_ENV === "development"
+
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [{
@@ -7,6 +9,13 @@ const nextConfig: NextConfig = {
       hostname: "gz0bl3nb3hcuttnc.public.blob.vercel-storage.com",
     }],
   },
+  /**
+   * @see {@link https://nextjs.org/docs/app/api-reference/config/next-config-js/reactCompiler}
+   */
+  reactCompiler: {
+    panicThreshold: isDevelopment ? "all_errors" : "none",
+    compilationMode: "infer",
+  }
 }
 
 export default nextConfig

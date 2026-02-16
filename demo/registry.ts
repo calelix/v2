cf24255e3161dcf5e464072de6509c774e2ccbd1
Jonggwang Park <jgpark7281@gmail.com>
2026-02-16 13:33:26 +0900

    feat: demo registry 추가

---
+++ demo/registry.ts
@@ -0,0 +1,18 @@
+import type { Metadata } from "next"
+
+import dynamic from "next/dynamic"
+
+export const registry: Record<string, {
+  name: string
+  Component: React.ComponentType
+  metadata: Metadata
+}> = {
+  sidebar01: {
+    name: "sidebar01",
+    metadata: {
+      title: "Sidebar 01",
+      description: "Sidebar with layout fixed header",
+    },
+    Component: dynamic(() => import("./sidebar/sidebar-01-page").then((m) => m.Sidebar01Page)),
+  },
+}
